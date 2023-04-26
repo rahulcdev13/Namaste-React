9669c8354c62c3649a360c52a89100cdd2f3174f
rahulcdev <reahulc.dev13@gmail.com>
2023-04-26 11:55:46 +0530

    namaste react

---
+++ App.js
@@ -0,0 +1,30 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React")
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading);
+
+// ================================
+
+{/* <div id="parent_child">
+    <div id="parent">
+        <div id="child">
+            <h1 id="child_parent">Hello World from React child parent</h1>
+            <h1 id="child">Hello World from React child</h1>
+        </div>
+    </div>
+</div> */}
+
+const parent = React.createElement(
+    "div", { id: "parent" },
+        React.createElement("div", { id: "child" },
+        React.createElement("h1", { id: 'child_parent' }, "Hello World from React child parent"),
+        React.createElement("h3", { id: 'child_parent' }, "Hello World from React child")
+        ),
+        React.createElement("div", { id: "child2" },
+        React.createElement("p", { id: 'child_parent' }, "Hello World from React child parent"),
+        React.createElement("p", { id: 'child_parent' }, "Hello World from React child")
+        )
+)
+console.log(parent);
+const parent_child = ReactDOM.createRoot(document.getElementById('parent_child'))
+parent_child.render(parent); 
---
+++ tech2 Support.md
@@ -0,0 +1,18 @@
+tech2 Support :
+
+1.What is Emmets ?
+- Emmet is the essential toolkit for web-developers
+- Emmet is mostly used for HTML, XML, and CSS, but it can also be used with programming languages. 
+
+2.what is CDN ?
+- A content delivery network (CDN) is a network of interconnected servers that speeds up webpage loading for data-heavy applications. 
+- CDN can stand for content delivery network or content distribution network.
+- better performance, increased reliability, cost savings, and resilience against cyber attacks.
+
+3. what is CrossOrigin ?
+- The crossorigin attribute sets the mode of the request to an HTTP CORS Request.
+- A cross-origin request is a request for a resource (e.g. style sheets, iframes, images, fonts, or scripts) from another domain.
+- CORS is used to manage cross-origin requests.
+
+
+
